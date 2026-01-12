fa8a671063d86372118533b9db38dfff3eccc423
Thane <thabay0606@students.maquoketaschools.org>
2026-01-12 14:01:48 -0600

    Defined public, class-level variables.
    Added fields for animal name, grade letter, year, height, and indoor pet status.

--- VariableTypes.cs
+++ VariableTypes.cs
@@ -2,4 +2,14 @@
 public class VariableTypes
 {   
     // Add your code here
+    public string AnimalName = "";
+    
+    public char GradeLetter = '';
+        
+    public int Year = ;
+        
+    public double Height = ;
+        
+    public bool IsIndoorPet = ;
 }
+
